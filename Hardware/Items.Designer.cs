d4c3f284ca87e5d43609273efd0eaabddfc6712d
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-06-12 11:33:15 -0300

    :sparkles: feat: Adicionando projeto de controle para loja de Hardware

--- Hardware/Items.Designer.cs
+++ Hardware/Items.Designer.cs
@@ -28,17 +28,56 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Items));
 			panel1 = new Panel();
+			panel2 = new Panel();
+			label1 = new Label();
+			pictureBox1 = new PictureBox();
+			panel1.SuspendLayout();
+			panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
 			SuspendLayout();
 			// 
 			// panel1
 			// 
 			panel1.BackColor = Color.Teal;
+			panel1.Controls.Add(panel2);
 			panel1.Dock = DockStyle.Left;
 			panel1.Location = new Point(0, 0);
 			panel1.Name = "panel1";
 			panel1.Size = new Size(244, 589);
 			panel1.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			panel2.BackColor = Color.DarkCyan;
+			panel2.Controls.Add(pictureBox1);
+			panel2.Controls.Add(label1);
+			panel2.Location = new Point(0, 531);
+			panel2.Name = "panel2";
+			panel2.Size = new Size(244, 58);
+			panel2.TabIndex = 2;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+			label1.ForeColor = Color.White;
+			label1.Location = new Point(83, 17);
+			label1.Name = "label1";
+			label1.Size = new Size(44, 25);
+			label1.TabIndex = 0;
+			label1.Text = "Sair";
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+			pictureBox1.Location = new Point(25, 8);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(45, 41);
+			pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+			pictureBox1.TabIndex = 2;
+			pictureBox1.TabStop = false;
 			// 
 			// Items
 			// 
@@ -51,11 +90,18 @@
 			Name = "Items";
 			StartPosition = FormStartPosition.CenterScreen;
 			Text = "Items";
+			panel1.ResumeLayout(false);
+			panel2.ResumeLayout(false);
+			panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
 			ResumeLayout(false);
 		}
 
 		#endregion
 
 		private Panel panel1;
+		private Panel panel2;
+		private Label label1;
+		private PictureBox pictureBox1;
 	}
 }
